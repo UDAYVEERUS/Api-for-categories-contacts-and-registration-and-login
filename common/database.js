4790d08a1077e62f9befd943271b34fb1de7af9c
Udayveer Singh <102755917+UDAYVEERUS@users.noreply.github.com>
2023-03-15 12:43:33 +0530

    change database

--- common/database.js
+++ common/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGOURL = 'mongodb://localhost:27017'
+const MONGOURL = 'mongodb+srv://udayveerus:8808613773@cluster0.i72qy8n.mongodb.net/test'
 
 const connectToMongo = () => {
     mongoose.connect(MONGOURL).then(console.log("connected successfully"))
